f5a4be042ee58fd3c5ed7a65227b6e82a7ffe297
eitanavgil <eitan.avgil@kaltura.com>
2019-06-26 22:49:43 +0300

    add spinner and cam

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
 import React from "react";
 import { Component } from "react";
+import Webcam from "react-webcam";
+import { SpiralSpinner } from "react-spinners-kit";
 
 import "./App.css";
 
 interface Props {}
-interface State {}
+interface State {
+  snap?: boolean;
+  loading?: boolean;
+  ks?: string;
+}
 
 class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSnap = this.handleSnap.bind(this);
+    this.state = { snap: false, loading: true };
+    this.getKs();
   }
 
   // async function always returns a Promise
-  async dramaticWelcome(): Promise<void> {
+  async getKs(): Promise<void> {
     await fetch("http://localhost/kaltura/?user=meme")
       .then(response => response.text())
-      .then(data => console.log(data));
+      .then(data => this.setState({ ks: data, loading: false }));
   }
 
-  handleSubmit() {
-    this.dramaticWelcome();
+  handleSnap() {
+    this.setState({ snap: true });
   }
 
   render() {
+    const { snap, ks, loading } = this.state;
     return (
       <div className="App">
+        <div className="spinner-wrapper">
+          <SpiralSpinner loading={loading} size={200} />
+        </div>
         <header className="App-header">
-          <button onClick={this.handleSubmit}> CALL ME </button>
+          {ks && <button onClick={this.handleSnap}>Snap me</button>}
+          {snap && <Webcam />}
         </header>
       </div>
     );
